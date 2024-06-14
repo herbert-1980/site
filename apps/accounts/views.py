46c28c1eb875266023535b05b6f9e40ac3ecfc26
Herbert Mesquita <herbertmesquita@gmail.com>
2024-06-14 01:45:19 -0300

    parte10_atualização_usuario

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import Group, User
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from accounts.forms import CustomUserCreationForm
-
+from django.contrib.auth.decorators import login_required
+from accounts.forms import UserChangeForm
+from accounts.models import MyUser
 
 # Create your views here.
 def timeout_view(request):
@@ -52,3 +54,30 @@ def register_view(request):
                 1 caractere especial e no minimo 8 caracteres.')
     form = CustomUserCreationForm()
     return render(request, "register.html",{"form": form})
+
+
+@login_required()
+def atualizar_meu_usuario(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Seu perfil foi atualizado com sucesso!')
+            return redirect('home')
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, 'user_update.html', {'form': form})
+
+
+@login_required()
+def atualizar_usuario(request, user_id):
+    user = get_object_or_404(MyUser, pk=user_id)
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'O perfil de usuário foi atualizado com sucesso!')
+            return redirect('home')
+    else:
+        form = UserChangeForm(instance=user)
+    return render(request, 'user_update.html', {'form': form})
